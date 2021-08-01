ed4db6f520039d2c249729a5bbbfdd5c0c29e5ba
Ayham Alahmed <81281479+AyhamAlahmed@users.noreply.github.com>
2021-08-01 01:03:31 -0400

    work on Milestone #2

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
@@ -20,6 +26,25 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedInputStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
     Button search;
@@ -30,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawer;
     NavigationView navigation;
     Intent intent;
+    private TextView articalNameEditText = null;
+    String articleName;
+    String title;
+    String date;
+    String iconName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +69,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         search = findViewById(R.id.search);
         editName = findViewById(R.id.newsSearch);
-        progressBar= findViewById(R.id.progressBar);
-        articles= findViewById(R.id.articles);
-        drawer= findViewById(R.id.drawer);
-        navigation= findViewById(R.id.navigation);
+        progressBar = findViewById(R.id.progressBar);
+        articles = findViewById(R.id.articles);
+        drawer = findViewById(R.id.drawer);
+        navigation = findViewById(R.id.navigation);
         instructions = findViewById(R.id.instructions);
+        articalNameEditText = findViewById(R.id.newsSearch);
+
+
 
         instructions.setOnClickListener(click ->
         {
@@ -57,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         navigation = findViewById(R.id.navigation);
         navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected( MenuItem ob) {
+            public boolean onNavigationItemSelected(MenuItem ob) {
 //                if (ob.getItemId() == R.id.nav_soccer) {
 //                    Toast.makeText(MainActivity.this, "You are using this app", Toast.LENGTH_LONG).show();
 //                    return true;
@@ -78,18 +113,18 @@ public class MainActivity extends AppCompatActivity {
 //                    return true;
 //                    }
 //                }
-            switch (ob.getItemId()) {
-                case R.id.nav_soccer :
-                     intent = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    Toast.makeText(MainActivity.this, "You are using this app", Toast.LENGTH_LONG).show();
-                break;
-                case R.id.nav_bus :
-                    intent = new Intent(MainActivity.this, OCTranspoApp.class);
-                    startActivity(intent);
+                switch (ob.getItemId()) {
+                    case R.id.nav_soccer:
+                        intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this, "You are using this app", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.nav_bus:
+                        intent = new Intent(MainActivity.this, OCTranspoApp.class);
+                        startActivity(intent);
 
-                break;
-            }
+                        break;
+                }
 
                 return false;
             }
@@ -100,7 +135,15 @@ public class MainActivity extends AppCompatActivity {
         prefs.getString("VariableName", defaultValue);
         String name = prefs.getString("Name", "");
         editName.setText(name);
+
         search.setOnClickListener(clicked -> {
+
+
+
+
+
+//            articleName =  articalNameEditText.getText().toString();
+
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("Name", editName.getText().toString());
             editor.apply();
@@ -109,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             int duration = Toast.LENGTH_LONG;
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setMessage("Welcome To sport News")
                     .setTitle("Soccer Game News")
                     .setNegativeButton("Cancel", (dialog, cl) -> {
@@ -121,8 +164,98 @@ public class MainActivity extends AppCompatActivity {
                             .show();
                 })
                     .create().show();
+
+
+//            AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+//                    .setTitle("Sport News")
+//                    .setMessage("We are calling people in " + articleName + "to look for their sport team news .")
+//                    .setView(new ProgressBar(MainActivity.this))
+//                    .show();
+//
+//        Executor newThread = Executors.newSingleThreadExecutor();
+//        newThread.execute(() -> {
+//            try {
+//                String serverURL = "https://api.openweathermap.org/data/2.5/weather?q="
+//                        + URLEncoder.encode(articleName, "UTF-8")
+//                        + "&appid=7e943c97096a9784391a981c4d878b22&units=metric&mode=xml";
+//
+//                URL url = new URL(serverURL);
+//                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//
+//                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+//                factory.setNamespaceAware(false);
+//                XmlPullParser xpp = factory.newPullParser();
+//                xpp.setInput(in, "UTF-8");
+//
+//                while (xpp.next() != XmlPullParser.END_DOCUMENT) {
+//                    switch (xpp.getEventType()) {
+//                        case XmlPullParser.START_TAG:
+//                            if (xpp.getName().equals("taitl")) {
+//                                title = xpp.getAttributeValue(null, "taitl");  //this gets the current temperature
+//                                date = xpp.getAttributeValue(null, "date"); //this gets the min temperature
+//
+//                            } else if (xpp.getName().equals("sport")) {
+//                                iconName = xpp.getAttributeValue(null, "icon"); //this gets the icon name
+//                            }
+//                            break;
+//
+//                        case XmlPullParser.END_TAG:
+//                            break;
+//                        case XmlPullParser.TEXT:
+//                            break;
+//                    }
+//
+//                }
+//                Bitmap image = null;
+//                File file = new File(getFilesDir(), iconName + ".png");
+//                if (file.exists()) {
+//                    image = BitmapFactory.decodeFile(getFilesDir() + "/" + iconName + ".png");
+//                } else {
+//                    URL imgUrl = new URL("https://openweathermap.org/img/w/" + iconName + ".png");
+//                    HttpURLConnection connection = (HttpURLConnection) imgUrl.openConnection();
+//                    connection.connect();
+//                    int responseCode = connection.getResponseCode();
+//                    if (responseCode == 200) {
+//                        image = BitmapFactory.decodeStream(connection.getInputStream());
+//                        image.compress(Bitmap.CompressFormat.PNG, 100, openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
+//                        ImageView imageView = findViewById(R.id.icon);
+//                        imageView.setImageBitmap(image);
+//                    }
+//                }
+//
+//                Bitmap finalImage = image;
+//                Bitmap finalImage1 = image;
+//                runOnUiThread(() -> {
+////                    TextView textView = findViewById(R.id.temp);
+////                    textView.setText("The current Temperature is " + title);
+////                    textView.setVisibility(View.VISIBLE);
+////
+////                    textView = findViewById(R.id.minTemp);
+////                    textView.setText("The Minimum Temperature is " + date);
+////                    textView.setVisibility(View.VISIBLE);
+//
+//
+//                    ImageView imageView = findViewById(R.id.icon);
+//                    imageView.setImageBitmap(finalImage);
+//                    imageView.setVisibility(View.VISIBLE);
+//                    imageView.setImageBitmap(finalImage1);
+//                    dialog.hide();
+//                });
+//
+//                FileOutputStream fOut = null;
+//                fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+//                image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+//                fOut.flush();
+//                fOut.close();
+//
+//            } catch (IOException | XmlPullParserException ieo) {
+//                Log.e("Connection error:", ieo.getMessage());
+//            }
+//        });
         });
     }
+
     @Override
     protected void onStart() {
         super.onStart();
